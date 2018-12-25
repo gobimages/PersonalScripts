76628fa791aba3673dbd47e74fe4a4fe33e6cd90
gobimages <32836630+gobimages@users.noreply.github.com>
2018-12-25 16:59:35 +0400

    deleted duplicate

--- gobby.ps1
+++ gobby.ps1
@@ -1,4 +1,4 @@
 $Secure_String_Pwd = ConvertTo-SecureString "P@ssW0rD!" -AsPlainText -Force
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDEployment
-Install-ADDSForest Install-ADDSForest -SafeModeAdministratorPassword $Secure_String_Pwd -CreateDnsDelegation:$false -DatabasePath 'C:\Windows\NTDS' -DomainMode 'Win2012R2' -DomainName 'somethingnearyou.com' -DomainNetbiosName 'something' -ForestMode 'Win2012R2' -InstallDns:$true -LogPath 'C:\Windows\NTDS' -NoRebootOnCompletion:$false -SysvolPath 'C:\Windows\SYSVOL' -Force:$true
+Install-ADDSForest -SafeModeAdministratorPassword $Secure_String_Pwd -CreateDnsDelegation:$false -DatabasePath 'C:\Windows\NTDS' -DomainMode 'Win2012R2' -DomainName 'somethingnearyou.com' -DomainNetbiosName 'something' -ForestMode 'Win2012R2' -InstallDns:$true -LogPath 'C:\Windows\NTDS' -NoRebootOnCompletion:$false -SysvolPath 'C:\Windows\SYSVOL' -Force:$true
