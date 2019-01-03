862c7784afa731746890ff3caddaa20249ef6c0b
gobimages <32836630+gobimages@users.noreply.github.com>
2019-01-03 11:52:52 +0400

    Update joindomain.ps1

--- joindomain.ps1
+++ joindomain.ps1
@@ -1,5 +1,5 @@
-$domain = "dpwho.ad"
-$password = "D9wDom@in@dm!n$" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\dcadmin" 
+$domain = "somethingnearyou.com"
+$password = "Mageswaran225" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\gobinath" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential
